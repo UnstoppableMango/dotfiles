2ceb8a29faeffc0ca0ce266b9480c8dbba4d29c5
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-10-28 20:18:58 -0500

    Enable nix-ld

--- hosts/hades/configuration.nix
+++ hosts/hades/configuration.nix
@@ -600,6 +600,35 @@
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
+  # https://github.com/NixOS/nixpkgs/issues/240444#issuecomment-1977617644
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    curl
+    expat
+    fontconfig
+    freetype
+    fuse
+    fuse3
+    glib
+    icu
+    libclang.lib
+    libdbusmenu
+    libxcrypt-legacy
+    libxml2
+    nss
+    openssl
+    python3
+    stdenv.cc.cc
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXext
+    xorg.libXi
+    xorg.libXrender
+    xorg.libXtst
+    xz
+    zlib
+  ];
+
   programs.firefox.enable = false; # Only blue fox
   programs.steam.enable = true;
 
