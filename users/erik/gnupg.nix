969020eaa38fd52126e4053f87aa74f410adc248
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-01-17 11:36:57 -0600

    Set gpg-agent pinentry

--- users/erik/gnupg.nix
+++ users/erik/gnupg.nix
@@ -1,10 +1,14 @@
 {
-  flake.modules.homeManager.erik = {
+  flake.modules.homeManager.erik = { pkgs, ... }: {
     programs.gpg.enable = true;
     services.gpg-agent = {
       enable = true;
       enableSshSupport = true;
       enableZshIntegration = true;
+      pinentry = {
+        package = pkgs.pinentry-all;
+        program = "pinentry-gnome3";
+      };
     };
   };
 }
